7a5c78730122c26fa0b345f6218bd4e94f2eb951
Frankie Mercado <oeluer@gmail.com>
2020-12-07 14:45:51 -0800

    restructure directories

---
+++ build/_dist_/index.js
@@ -0,0 +1,3 @@
+import React from "../web_modules/react.js";
+import ReactDOM from "../web_modules/react-dom.js";
+ReactDOM.render(/* @__PURE__ */ React.createElement("div", null, '"HELLO REACT"'), document.getElementById("root"));
