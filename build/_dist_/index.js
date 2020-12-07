32a4d490cfcd4cbec0766d69c330fd57f97349cf
Frankie Mercado <oeluer@gmail.com>
2020-12-07 14:55:59 -0800

    create App component

--- build/_dist_/index.js
+++ build/_dist_/index.js
@@ -1,3 +1,4 @@
 import React from "../web_modules/react.js";
 import ReactDOM from "../web_modules/react-dom.js";
-ReactDOM.render(/* @__PURE__ */ React.createElement("div", null, '"HELLO REACT"'), document.getElementById("root"));
+import App2 from "./App.js";
+ReactDOM.render(/* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(App2, null)), document.getElementById("root"));
